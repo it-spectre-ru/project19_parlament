6243acd152bdf8fc8200aade777c4a8dc57bd2e4
s <s>
2022-07-01 20:14:31 +0500

    done with parce error (maybe timeout)

--- main.py
+++ main.py
@@ -1,53 +1,58 @@
-from unittest import result
 import requests
 from bs4 import BeautifulSoup
+import json
 
-
-# person_url_list = []
-
+# persons_url_list = []
+#
 # for i in range(0, 740, 20):
-#   url = f'https://www.bundestag.de/ajax/filterlist/en/members/863330-863330?limit=20&noFilterSet=true&offset={i}'
-  
-#   q = requests.get(url)
-#   result = q.content
-
-#   soup = BeautifulSoup(result, 'lxml')
-#   persons = soup.find_all(class_='bt-open-in-overlay')
-
-#   for person in persons:
-#     person_page_url = person.get('href')
-#     person_url_list.append(person_page_url)
-    
-# with open('persona_url_list.txt', 'a') as file:
-#   for line in person_url_list:
-#     file.write(f'{line}\n')
-
-
-# with open('persona_url_list.txt') as file:
-
-#   lines = [line.strip() for line in file.readlines()]
-
-#   for line in lines:
-#     q = requests.get(line)
+#     url = f'https://www.bundestag.de/ajax/filterlist/en/members/453158-453158/h_a45203fd0f1592191f1bda63b5d86d72?limit=20&noFilterSet=true&offset={i}'
+#
+#     q = requests.get(url)
 #     result = q.content
-
+#
 #     soup = BeautifulSoup(result, 'lxml')
-#     person = soup.find(class_='bt-biografie-name').find('h3').text
-#     person_name_company = person.strip().split(',')
-#     person_name = person_name_company[0]
-#     person_company = person_name_company[1]
+#     persons = soup.find_all(class_='bt-open-in-overlay')
+#
+#     for person in persons:
+#         person_page_url = person.get('href')
+#         persons_url_list.append(person_page_url)
+#
+# with open('persons_url_list.txt', 'a') as file:
+#     for line in persons_url_list:
+#         file.write(f'{line}\n')
 
+with open('persona_url_list.txt') as file:
 
+    lines = [line.strip() for line in file.readlines()]
 
-q = requests.get('https://www.bundestag.de/en/members/zimmermann_jens-858214')
-result = q.content
+    data_dict = []
+    count = 0
 
-soup = BeautifulSoup(result, 'lxml')
-person = soup.find(class_='bt-biografie-name').find('h3').text
-person_name_company = person.strip().split(',')
-person_name = person_name_company[0]
-person_company = person_name_company[1]
+    for line in lines:
+        q = requests.get(line)
+        result = q.content
 
-print(person_name)
-print(person_company)
+        soup = BeautifulSoup(result, 'lxml')
+        person = soup.find(class_='bt-biografie-name').find('h3').text
+        person_name_company = person.strip().split(',')
+        person_name = person_name_company[0]
+        person_company = person_name_company[1].strip()
 
+        social_networks = soup.find_all(class_='bt-link-extern')
+
+        social_networks_urls = []
+        for item in social_networks:
+            social_networks_urls.append(item.get('href'))
+
+        data = {
+            'person_name': person_name,
+            'company_name': person_company,
+            'social_networks': social_networks_urls
+        }
+        count += 1
+        print(f'#{count}: {line} is done!')
+
+        data_dict.append(data)
+
+        with open('data.json', 'w') as json_file:
+            json.dump(data_dict, json_file, indent=4)

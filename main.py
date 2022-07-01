b5924fe9bfc4a6c67c1d5c7c5b33be97ba4ae095
s <s>
2022-07-01 19:40:58 +0500

    get link list and write in txt them

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+person_url_list = []
+
 for i in range(0, 740, 20):
   url = f'https://www.bundestag.de/ajax/filterlist/en/members/863330-863330?limit=20&noFilterSet=true&offset={i}'
-  print(url)
+  
+  q = requests.get(url)
+  result = q.content
+
+  soup = BeautifulSoup(result, 'lxml')
+  persons = soup.find_all(class_='bt-open-in-overlay')
+
+  for person in persons:
+    person_page_url = person.get('href')
+    person_url_list.append(person_page_url)
+    
+with open('persona_url_list.txt', 'a') as file:
+  for line in person_url_list:
+    file.write(f'{line}\n')
